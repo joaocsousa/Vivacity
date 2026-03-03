70f04d247cd8cc151a52af81eb893d9ff3b8ca6f
Joao Sousa <joaocsousa@users.noreply.github.com>
2026-03-03 23:28:58 +0000

    Fix Release build by gating fake environment code

--- Vivacity/Utilities/AppEnvironment.swift
+++ Vivacity/Utilities/AppEnvironment.swift
@@ -13,6 +13,7 @@ enum AppEnvironment {
 
     /// Call at app startup to configure fakes when running UI tests.
     static func configureForTestingIfNeeded() {
+#if DEBUG
         let env = ProcessInfo.processInfo.environment
         guard env["VIVACITY_USE_FAKE_SERVICES"] == "1" else { return }
 
@@ -33,6 +34,7 @@ enum AppEnvironment {
                 ])
             )
         }
+#endif
     }
 }
 
